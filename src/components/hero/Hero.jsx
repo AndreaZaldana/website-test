c0d8b375fa4a584d5fb1b654e6376ef2ba91e8c4
Andrea <zaldanafp@gmail.com>
2024-05-29 17:56:23 -0600

    updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
   return (
     <div className='hero container' id='heroSection' ref={heroRef}>
         <div className="hero-text">
-            <h1>CRZ</h1>
+            <h1><span className='highlight'>C</span>onstruímos tu camino con p<span className='highlight'>r</span>ecisión para que
+            condu<span className='highlight'>z</span>cas con seguridad.</h1>
             <p>Nuestro principal objetivo es ser una empresa con sensibilidad humana, aportando elementos
             fundamentales, que brinden soluciones a las diferentes variables que se presentan, teniendo como
             marco la óptica social y la protección al medio ambiente en cada uno de los proyectos a realizar.
